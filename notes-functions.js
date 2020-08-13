9dc5eae736bf9b0a10640a58f5a006b8cb5d906f
David Blackwelder <github@davidblackwelder.com>
2020-08-13 07:03:22 -0400

    Add button to note element

--- notes-functions.js
+++ notes-functions.js
@@ -18,12 +18,17 @@ const saveNotes = function (notes) {
 // Generate the DOM structure for a note
 const generateNoteDOM = function (note) {
   const noteEl = document.createElement("p");
+  const button = document.createElement("button");
+
+  button.textContent = "x";
 
   if (note.title.length > 0) {
     noteEl.textContent = note.title;
   } else {
     noteEl.textContent = "Unnamed note";
   }
+
+  noteEl.appendChild(button);
 
   return noteEl;
 };
